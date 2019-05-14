f83877ef9ecb91e83f1573c16fee583beef869fa
Julia Kwiecien <julia.kwiecien@hatimeria.com>
2019-05-14 17:52:52 +0200

    Add basic styles, add styles to hero banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,13 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import HeroBanner from "../components/herobanner"
 
 const IndexPage = ({ location }) => (
 
   <Layout location={location}>
     <SEO title="Metodyki CSS - BEM" keywords={[`css`, `metodyki css`, `bem`, `bem css`, `css methodologies`]} />
-    <h1>BEM</h1>
+    <HeroBanner/>
   </Layout>
 )
 
