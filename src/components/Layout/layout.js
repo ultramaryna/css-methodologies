db16f48366923ba573376bfa18b2a99cff9782aa
Julia Kwiecien <julia.kwiecien@hatimeria.com>
2019-06-11 20:02:40 +0200

    Add scroll to top button

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -3,12 +3,14 @@ import PropTypes from "prop-types"
 
 import Header from '../Header/header'
 import Footer from '../Footer/footer'
+import ScrollToTop from '../ScrollToTop/scrollToTop';
 
 const Layout = ({ children }) => (
   <>
     <Header />
     <main>{children}</main>
     <Footer />
+    <ScrollToTop />
   </>
 )
 
---
+++ src/components/ScrollToTop/scrollToTop.js
@@ -0,0 +1,35 @@
+import React from "react"
+
+class ScrollToTop extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isVisible: false
+    }
+  }
+
+  componentDidMount() {
+    const self = this;
+
+    window.addEventListener('scroll', function() {
+      const scrollPosition = this.scrollY / document.body.scrollHeight;
+      if (scrollPosition > 0.4) {
+        self.setState({isVisible: true});
+      }
+    });
+  }
+
+  scrollToTop() {
+    window.scrollTo(0,0);
+  }
+
+  render() {
+    return (
+      <button className="scroll-top" type="button" onClick={() => this.scrollToTop()}>
+        <i className="icon-chevron" />
+      </button>
+    );
+  }
+}
+
+export default ScrollToTop
