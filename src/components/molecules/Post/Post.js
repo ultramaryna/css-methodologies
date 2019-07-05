bc29ca5ea362bf2decd1f920cd377c7972dd0888
Julia Kwiecien <julia.kwiecien@hatimeria.com>
2019-07-05 08:17:47 +0200

    Add reset css, add post molecule

--- src/components/molecules/Post/Post.js
+++ src/components/molecules/Post/Post.js
@@ -4,12 +4,27 @@ import styles from "./styles.scss";
 import Image from '../../atoms/Image/Image';
 import Subtitle from '../../atoms/Subtitle/Subtitle';
 import Text from '../../atoms/Text/Text';
+import LinkBtn from '../../atoms/LinkBtn/LinkBtn';
 
-const Post = ({ title, image, type, children }) => (
-  <article className="post">
-    <Image src={image} alt={title} />
-    <Subtitle isLowercase>{title}</Subtitle>
-    <Text>{children}</Text>
+const Post = ({ title, image, type, link, children }) => (
+  <article className={`post ${type}`}>
+    <Image src={image} alt={image} className="image"/>
+    <div className="content">
+      <Subtitle
+        className="title"
+        isLowercase={type === 'vertical'}
+        color={type === 'horizontal' ? 'grey' : ''}
+      >
+        {title}
+      </Subtitle>
+      <Text className="excerpt">{children}</Text>
+      <LinkBtn
+        link={link}
+        variant={type === 'vertical' ? 'secondary' : 'primary'}
+      >
+        Czytaj
+      </LinkBtn>
+    </div>
   </article>
 )
 
--- src/components/molecules/Post/stories.js
+++ src/components/molecules/Post/stories.js
@@ -4,11 +4,17 @@ import { storiesOf } from '@storybook/react';
 import { themes } from '@storybook/theming';
 
 import Post from './Post';
+import image from '../../../../static/images/posts/ghost.jpg';
 
 storiesOf('Post', module)
   .addParameters({ options: { theme: themes.dark } })
+  .add('vertical', () =>
+    <Post type="vertical" image={image} title="Lorem ipsum">
+      Lorem ipsum
+    </Post>
+  )
   .add('horizontal', () =>
-    <Post type="horizontal" image="/" title="Lorem ipsum">
-      Content
+    <Post type="horizontal" image={image} title="Lorem ipsum">
+      Lorem ipsum
     </Post>
   );
