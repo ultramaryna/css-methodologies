21eb4953b32e8629f9ff9f94fcd7bbc59eabc754
Julia Kwiecien <julia.kwiecien@hatimeria.com>
2019-08-17 17:35:03 +0200

    Add links in footer, fix footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -2,13 +2,19 @@ import React from "react";
 
 const Footer = () => (
   <footer className="footer">
-    <p className="footer__text">
-      Autorem niniejszego serwisu jest Julia Kwiecień.<br/>
-      Serwis ten stanowi integralną część pracy magisterskiej (kierunek: Elektroniczne Przetwarzanie Informacji), przygotowanej pod kierunkiem prof. dr. hab. Mariusza Flasińskiego na Wydziale Zarządzania i Komunikacji Społecznej Uniwersytetu Jagiellońskiego.
-    </p>
-    <p className="footer__version">
-      Wersja BEM
-    </p>
+    <div className="footer__content">
+      <p className="footer__text">
+        Autorem niniejszego serwisu jest Julia Kwiecień.<br/>
+        Serwis ten stanowi integralną część pracy magisterskiej (kierunek: Elektroniczne Przetwarzanie Informacji), przygotowanej pod kierunkiem prof. dr. hab. Mariusza Flasińskiego na Wydziale Zarządzania i Komunikacji Społecznej Uniwersytetu Jagiellońskiego.
+      </p>
+      <p className="footer__version">
+        Wersja BEM
+      </p>
+      <div className="footer__links">
+        <a className="footer__link" href="http://leszczyna.wzks.uj.edu.pl/~14_kwiecien/metodologie/atomic-design">Atomic Design</a>
+        <a className="footer__link" href="http://leszczyna.wzks.uj.edu.pl/~14_kwiecien/metodologie/css-in-js">CSS-in-JS</a>
+      </div>
+    </div>
   </footer>
 )
 
