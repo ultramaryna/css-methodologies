5d4b0a7b0ef7f7fbe07c8eb804499d3468922daf
Julia Kwiecien <julia.kwiecien@hatimeria.com>
2019-08-17 11:45:44 +0200

    Fix footer, add links to other versions

--- src/components/organisms/Footer/Footer.js
+++ src/components/organisms/Footer/Footer.js
@@ -3,13 +3,19 @@ import Text from "../../atoms/Text/Text";
 
 const Footer = () => (
   <footer className="o-footer">
-      <Text extraClass="o-footer_text">
-      Autorem niniejszego serwisu jest Julia Kwiecień.<br/>
-      Serwis ten stanowi integralną część pracy magisterskiej (kierunek: Elektroniczne Przetwarzanie Informacji), przygotowanej pod kierunkiem prof. dr. hab. Mariusza Flasińskiego na Wydziale Zarządzania i Komunikacji Społecznej Uniwersytetu Jagiellońskiego.
-      </Text>
-    <p className="o-footer_version">
-      Wersja Atomic Design
-    </p>
+      <div className="o-footer_content">
+        <Text extraClass="o-footer_text">
+        Autorem niniejszego porównania jest Julia Kwiecień.<br/>
+        Aplikacja stanowi integralną część pracy magisterskiej (kierunek: Elektroniczne Przetwarzanie Informacji), przygotowanej pod kierunkiem prof. dr. hab. Mariusza Flasińskiego na Wydziale Zarządzania i Komunikacji Społecznej Uniwersytetu Jagiellońskiego.
+        </Text>
+        <p className="o-footer_version">
+          Wersja Atomic Design
+        </p>
+        <div className="o-footer_links">
+          <a className="o-footer_link" href="http://leszczyna.wzks.uj.edu.pl/~14_kwiecien/metodologie/bem">BEM</a>
+          <a className="o-footer_link" href="http://leszczyna.wzks.uj.edu.pl/~14_kwiecien/metodologie/css-in-js">CSS-in-JS</a>
+        </div>
+      </div>
   </footer>
 )
 
